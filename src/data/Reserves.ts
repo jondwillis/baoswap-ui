d0d3458f61d277d9eb955416b7c617cf041799d9
Jon Willis <jondwillis@gmail.com>
2021-03-16 20:45:04 -0400

    useUserInfoPairFarmablePools

--- src/data/Reserves.ts
+++ src/data/Reserves.ts
@@ -1,4 +1,4 @@
-import { TokenAmount, Pair, Currency } from 'uniswap-xdai-sdk'
+import { TokenAmount, Pair, Currency, Token, ChainId, WETH } from 'uniswap-xdai-sdk'
 import { useMemo } from 'react'
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { Interface } from '@ethersproject/abi'
@@ -8,7 +8,7 @@ import { useMultipleContractSingleData, useSingleContractMultipleData } from '..
 import { wrappedCurrency } from '../utils/wrappedCurrency'
 import { useMasterChefContract } from '../hooks/useContract'
 import { PairFarmablePool } from '../components/PositionCard'
-import { useSupportedLpTokenMap } from '../bao/lib/constants'
+import { contractAddresses, useSupportedLpTokenMap } from '../bao/lib/constants'
 
 const PAIR_INTERFACE = new Interface(IUniswapV2PairABI)
 
@@ -77,44 +77,55 @@ export function useAllV2PairsWithLiquidity(v2Pairs: [PairState, Pair | null][]):
   }, [v2Pairs, supportedLpTokenMap])
 }
 
-export function useAllStaked(currencies: [Currency | undefined, Currency | undefined][]): [PairState, Pair | null][] {
+export interface UserInfoPairFarmablePool extends PairFarmablePool {
+  stakedAmount: TokenAmount
+  rewardDebt: TokenAmount
+}
+
+export function useUserInfoPairFarmablePools(
+  pairFarmablePools: PairFarmablePool[]
+): UserInfoPairFarmablePool[] | undefined {
   const { chainId } = useActiveWeb3React()
   const masterChefContract = useMasterChefContract()
 
-  const tokens = useMemo(
-    () =>
-      currencies.map(([currencyA, currencyB]) => [
-        wrappedCurrency(currencyA, chainId),
-        wrappedCurrency(currencyB, chainId)
-      ]),
-    [chainId, currencies]
-  )
+  const chainIdNumber = chainId == ChainId.XDAI ? 100 : 4
+  const baoRewardToken = (chainId && new Token(chainId, contractAddresses.bao[chainIdNumber], 18)) || WETH[100]
 
-  // const pairAddresses = useMemo(
-  //   () =>
-  //     tokens.map(([tokenA, tokenB]) => {
-  //       return tokenA && tokenB && !tokenA.equals(tokenB) ? Pair.getAddress(tokenA, tokenB) : undefined
-  //     }),
-  //   [tokens]
-  // )
-
-  const results = useSingleContractMultipleData(masterChefContract, 'userInfo', [])
-
-  return useMemo(() => {
-    return results.map((result, i) => {
-      const { result: reserves, loading } = result
-      const tokenA = tokens[i][0]
-      const tokenB = tokens[i][1]
-
-      if (loading) return [PairState.LOADING, null]
-      if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
-      if (!reserves) return [PairState.NOT_EXISTS, null]
-      const { reserve0, reserve1 } = reserves
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
-      return [
-        PairState.EXISTS,
-        new Pair(new TokenAmount(token0, reserve0.toString()), new TokenAmount(token1, reserve1.toString()))
-      ]
+  const poolIdsAndLpTokens = useMemo(() => {
+    const matrix = pairFarmablePools.map(tuple => {
+      const { pair, farmablePool } = tuple
+      return [farmablePool.pid, pair.liquidityToken.address]
     })
-  }, [results, tokens])
+    return matrix
+  }, [pairFarmablePools])
+
+  const results = useSingleContractMultipleData(masterChefContract, 'userInfo', poolIdsAndLpTokens)
+
+  const userInfoPairFarmablePool = useMemo(() => {
+    return pairFarmablePools.map((tuple, i) => {
+      const { pair, farmablePool } = tuple
+      const stakedAmountResult = results?.[i]?.result?.[0]
+      const rewardDebtAmount = results?.[i]?.result?.[1]
+
+      const mergeObject =
+        stakedAmountResult && rewardDebtAmount
+          ? {
+              stakedAmount: new TokenAmount(pair.liquidityToken, stakedAmountResult),
+              rewardDebt: new TokenAmount(baoRewardToken, rewardDebtAmount)
+            }
+          : {
+              stakedAmount: new TokenAmount(pair.liquidityToken, '0'),
+              rewardDebt: new TokenAmount(baoRewardToken, '0')
+            }
+
+      return {
+        pair,
+        farmablePool,
+        stakedAmount: mergeObject.stakedAmount,
+        rewardDebt: mergeObject.rewardDebt
+      }
+    })
+  }, [pairFarmablePools, results, baoRewardToken])
+
+  return userInfoPairFarmablePool
 }
