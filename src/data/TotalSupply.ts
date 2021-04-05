63758270fa8eaaf2dbb671d033314760995bddfd
Jon Willis <jondwillis@gmail.com>
2021-04-05 10:35:33 -0400

    memoize totalSupply

--- src/data/TotalSupply.ts
+++ src/data/TotalSupply.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from '@ethersproject/bignumber'
+import { useMemo } from 'react'
 import { Token, TokenAmount } from 'uniswap-xdai-sdk'
 import { useTokenContract } from '../hooks/useContract'
 import { useSingleCallResult } from '../state/multicall/hooks'
@@ -8,7 +8,7 @@ import { useSingleCallResult } from '../state/multicall/hooks'
 export function useTotalSupply(token?: Token): TokenAmount | undefined {
   const contract = useTokenContract(token?.address, false)
 
-  const totalSupply: BigNumber = useSingleCallResult(contract, 'totalSupply')?.result?.[0]
+  const totalSupply: string | undefined = useSingleCallResult(contract, 'totalSupply')?.result?.[0]
 
-  return token && totalSupply ? new TokenAmount(token, totalSupply.toString()) : undefined
+  return useMemo(() => (token && totalSupply ? new TokenAmount(token, totalSupply) : undefined), [totalSupply, token])
 }
