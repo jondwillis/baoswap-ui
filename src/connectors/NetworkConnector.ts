84a75e142d7143f97e447d1db8276befb42ab538
Jon Willis <jondwillis@gmail.com>
2021-04-05 13:47:44 -0400

    batch RPC network calls rate limited at 5s

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -47,7 +47,7 @@ class MiniRpcProvider implements AsyncSendable {
     this.host = parsed.host
     this.path = parsed.pathname
     // how long to wait to batch calls
-    this.batchWaitTimeMs = batchWaitTimeMs ?? 50
+    this.batchWaitTimeMs = batchWaitTimeMs ?? 5000
   }
 
   public readonly clearBatch = async () => {
