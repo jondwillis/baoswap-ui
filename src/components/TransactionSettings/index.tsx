214813a69bfcea48428345e9cc3c60bb4ee96c52
Jon Willis <jondwillis@gmail.com>
2021-04-09 18:22:19 -0400

    slip Option et al out of TransactionSettings

--- src/components/TransactionSettings/index.tsx
+++ src/components/TransactionSettings/index.tsx
@@ -5,8 +5,7 @@ import QuestionHelper from '../QuestionHelper'
 import { TYPE } from '../../theme'
 import { AutoColumn } from '../Column'
 import { RowBetween, RowFixed } from '../Row'
-
-import { darken } from 'polished'
+import { Option, OptionCustom } from '../RadioButton'
 
 enum SlippageError {
   InvalidInput = 'InvalidInput',
@@ -18,32 +17,11 @@ enum DeadlineError {
   InvalidInput = 'InvalidInput'
 }
 
-const FancyButton = styled.button`
-  color: ${({ theme }) => theme.text1};
-  align-items: center;
-  height: 2rem;
-  border-radius: 36px;
-  font-size: 12px;
-  width: auto;
-  min-width: 3rem;
-  border: 1px solid ${({ theme }) => theme.bg3};
-  outline: none;
-  background: ${({ theme }) => theme.bg1};
-  :hover {
-    border: 1px solid ${({ theme }) => theme.bg4};
-  }
-  :focus {
-    border: 1px solid ${({ theme }) => theme.primary1};
-  }
-`
-
-const Option = styled(FancyButton)<{ active: boolean }>`
-  margin-right: 8px;
-  :hover {
-    cursor: pointer;
-  }
-  background-color: ${({ active, theme }) => active && theme.primary1};
-  color: ${({ active, theme }) => (active ? theme.primaryText1 : theme.text1)};
+const SlippageEmojiContainer = styled.span`
+  color: #f3841e;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    display: none;  
+  `}
 `
 
 const Input = styled.input`
@@ -57,32 +35,6 @@ const Input = styled.input`
   }
   color: ${({ theme, color }) => (color === 'red' ? theme.red1 : theme.text1)};
   text-align: right;
-`
-
-const OptionCustom = styled(FancyButton)<{ active?: boolean; warning?: boolean }>`
-  height: 2rem;
-  position: relative;
-  padding: 0 0.75rem;
-  flex: 1;
-  border: ${({ theme, active, warning }) => active && `1px solid ${warning ? theme.red1 : theme.primary1}`};
-  :hover {
-    border: ${({ theme, active, warning }) =>
-      active && `1px solid ${warning ? darken(0.1, theme.red1) : darken(0.1, theme.primary1)}`};
-  }
-
-  input {
-    width: 100%;
-    height: 100%;
-    border: 0px;
-    border-radius: 2rem;
-  }
-`
-
-const SlippageEmojiContainer = styled.span`
-  color: #f3841e;
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    display: none;  
-  `}
 `
 
 export interface SlippageTabsProps {
