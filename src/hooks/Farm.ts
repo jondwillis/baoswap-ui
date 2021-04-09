0dbb63e366f6f03a84132069219528c49e8623cd
Jon Willis <jondwillis@gmail.com>
2021-04-09 17:59:48 -0400

    better async handling of harvest all

--- src/hooks/Farm.ts
+++ src/hooks/Farm.ts
@@ -15,30 +15,34 @@ export function useHarvestAll(
 ): { state: HarvestState; callback?: null | (() => Promise<any[]>); error: string | null } {
   const masterChefContract = useMasterChefContract()
   const addTransaction = useTransactionAdder()
-
   return useMemo(() => {
     return {
       state: HarvestState.PENDING,
       callback:
         masterChefContract &&
         async function onHarvestAll(): Promise<any[]> {
-          return await Promise.all(
-            farmablePools.map(async farm => {
-              const txReceipt = await masterChefContract.claimReward(farm.pid)
-              addTransaction(txReceipt, { summary: `Harvest ${farm.name} (Pool ID: ${farm.pid})` })
-              const txHash = txReceipt.hash
-              return txHash
-            })
-          ).catch((error: any) => {
-            // if the user rejected the tx, pass this along
-            if (error?.code === 4001) {
-              throw new Error('Transaction rejected.')
-            } else {
-              // otherwise, the error was unexpected and we need to convey that
-              console.error(`Harvest failed`, error)
-              throw new Error(`Harvest failed: ${error.message}`)
-            }
+          const harvests = farmablePools.map(async farm => {
+            const txReceipt = await masterChefContract.claimReward(farm.pid)
+            addTransaction(txReceipt, { summary: `Harvest ${farm.name} (Pool ID: ${farm.pid})` })
+            const txHash = txReceipt.hash
+            return txHash
           })
+          return await harvests
+            .reduce(async (promiseChain, currentTask) => {
+              const chainResults = await promiseChain
+              const currentResult = await currentTask
+              return [...chainResults, currentResult]
+            }, Promise.resolve([]))
+            .catch((error: any) => {
+              // if the user rejected the tx, pass this along
+              if (error?.code === 4001) {
+                throw new Error('Transaction rejected.')
+              } else {
+                // otherwise, the error was unexpected and we need to convey that
+                console.error(`Harvest failed`, error)
+                throw new Error(`Harvest failed: ${error.message}`)
+              }
+            })
         },
       error: null
     }
